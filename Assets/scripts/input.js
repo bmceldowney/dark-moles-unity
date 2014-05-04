204688f3e670475d991725dc442a44fc384bfceb
Ben McEldowney <bmceldowney@gmail.com>
2014-05-03 22:07:57 -0700

    initial commit

---
+++ Assets/scripts/input.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var maxSpeed = 3;
+
+function Start () {
+	rigidbody2D.fixedAngle = true;
+}
+
+function Update () {
+	var from = transform.position;
+	var to = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+	var angle = Vector2.Angle(Vector2.right, to - from);
+	if (angle > 90) {
+		transform.localScale.x = -1;
+	} else {
+		transform.localScale.x = 1;
+	}
+
+	if (Input.GetKey ("w")) {
+		rigidbody2D.velocity.y = maxSpeed;
+	}
+	
+	if (Input.GetKey ("s")) {
+		rigidbody2D.velocity.y = -maxSpeed;
+	}
+	
+	if (Input.GetKey ("a")) {
+		rigidbody2D.velocity.x = -maxSpeed;
+	}
+
+	if (Input.GetKey ("d")) {
+		rigidbody2D.velocity.x = maxSpeed;
+	}
+
+		if (rigidbody2D.velocity.x <= maxSpeed || rigidbody2D.velocity.y <= maxSpeed) {
+		animation.Play("stand");
+	}else {
+		animation.Play("walk");
+	}
+}
+
+function OnGUI () {
+	GUI.TextArea(new Rect(10,10,100,30), rigidbody2D.velocity.x.ToString());
+}
---
+++ Assets/scripts/load.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+import SimpleJSON;
+
+function Start () {
+	var json : TextAsset = Resources.Load("levels", TextAsset);
+	var stages = JSON.Parse(json.text);
+	var firstStage = stages[0].Value;
+}
+
+function Update () {
+
+}
